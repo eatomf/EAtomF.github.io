190f4912875a6cc10900f1d0b661843c3ca5286d
EAtomF <2667892482@qq.com>
2026-06-17 11:23:48 +0800

    2026/6/17

--- Script/Login.js
+++ Script/Login.js
@@ -1,6 +1,7 @@
 const body = document.querySelector('body');
 let deg = 0;
 setInterval(() => {
-    body.style.background = `linear-gradient(${deg}deg, blue, red)`;
+    body.style.background = `linear-gradient(${deg}deg, aqua,blue)`;
+    body.style.backgroundSize = '100% 100%';
     deg = (deg + 1) % 360;
-}, 15)
+}, 15);
