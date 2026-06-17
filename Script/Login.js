1cdf6882ae5eacb20433af051faa4218701a514a
EAtomF <2667892482@qq.com>
2026-06-17 16:33:19 +0800

    大版本更新

--- Script/Login.js
+++
@@ -1,7 +0,0 @@
-const body = document.querySelector('body');
-let deg = 0;
-setInterval(() => {
-    body.style.background = `linear-gradient(${deg}deg, aqua,blue)`;
-    body.style.backgroundSize = '100% 100%';
-    deg = (deg + 1) % 360;
-}, 15);
